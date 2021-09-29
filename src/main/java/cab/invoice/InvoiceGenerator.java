ef64cd20498f0d26bfa246b8087e59e513305e3d
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-29 23:08:36 +0530

    test cases added and invoice summary added

---
+++ src/main/java/cab/invoice/InvoiceGenerator.java
@@ -0,0 +1,31 @@
+package cab.invoice;
+
+public class InvoiceGenerator {
+
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+	private static final double MINIMUM_FARE = 5;
+
+	private static final int COST_PER_TIME_PREMIUM = 2;
+	private static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15.0;
+	private static final double MINIMUM_FARE_PREMIUM = 20;
+
+	public double calculateFare(String rideCategory,double distance, int time) {
+		if(rideCategory.equals("premium")) {
+			double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+			return Math.max(totalFare,MINIMUM_FARE_PREMIUM);
+		}else {
+			double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+			return Math.max(totalFare,MINIMUM_FARE);
+		}
+	}
+
+	public InvoiceSummary calculateFare(String rideCategory,Ride[] rides) {
+		double totalFare = 0;
+		for(Ride ride:rides) {
+			totalFare += this.calculateFare(rideCategory,ride.distance,ride.time);
+		}
+		return new InvoiceSummary(rides.length,totalFare);
+	}
+
+}
---
+++ src/test/java/cab/invoice/InvoiceServiceTest.java
@@ -0,0 +1,79 @@
+package cab.invoice;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class InvoiceServiceTest {
+	InvoiceGenerator invoiceGenerator = null;
+	@BeforeEach
+	public void setUp() throws Exception{
+		invoiceGenerator = new InvoiceGenerator();
+	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare("premium",distance,time);
+		assertEquals(40,fare,0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare("normal",distance,time);
+		assertEquals(5,fare,0.0);
+	}
+
+	@Test
+	public void givenMultipleRide_ShouldReturnInvoiceSummary() {
+		Ride[] user1rides = { new Ride(2.0,5),
+				new Ride(0.1,1),
+				new Ride(2,10)
+		};
+		Ride[] user2rides = { new Ride(3.0,5),
+				new Ride(0.1,2)
+		};
+		Ride[] user3rides = { new Ride(2.0,10),
+				new Ride(0.1,1)
+		};
+		Map<Integer,Ride[]> rideRepository = new HashMap<>();
+		rideRepository.put(101, user1rides);
+		rideRepository.put(102, user2rides);
+		rideRepository.put(103, user3rides);
+		int userID = 101;
+		String rideCategory = "normal";
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rideCategory,rideRepository.get(userID));
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3,60.0);
+		assertEquals(expectedInvoiceSummary,summary);
+	}
+
+	@Test
+	public void givenMultipleRide_ShouldReturnInvoiceSummaryForPremiumRides() {
+		Ride[] user1rides = { new Ride(2.0,5),
+				new Ride(0.1,1),
+				new Ride(2,10)
+		};
+		Ride[] user2rides = { new Ride(3.0,5),
+				new Ride(0.1,2)
+		};
+		Ride[] user3rides = { new Ride(2.0,10),
+				new Ride(0.1,1)
+		};
+		Map<Integer,Ride[]> rideRepository = new HashMap<>();
+		rideRepository.put(101, user1rides);
+		rideRepository.put(102, user2rides);
+		rideRepository.put(103, user3rides);
+		int userID = 101;
+		String rideCategory = "premium";
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rideCategory,rideRepository.get(userID));
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3,110.0);
+		assertEquals(expectedInvoiceSummary,summary);
+	}
+}
+
+
